dd906625719f12f48c354546584203e279540400
MariaDelgadoEiriz2 <m.delgadoei@alumnos.urjc.es>
2019-02-03 12:31:29 +0100

    Estructura del Dashboard
    Hecha la lógica de la navegación por pestañas. Hay 3 divs habilitados para cada apartado fijo. Dejaremos los apartados dinámicos para mas adelante.

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,11 +1,13 @@
 function accesscontrol() {
+    console.log (user);
+    console.log (password);
     var user = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if ((user == "grupo12") && (password == "1234")) {
-        // var url = "prueba.html";
-        //$(location).attr('href', url);
+        var url = "dashboard.html";
+        $(location).attr('href', url);
     } else {
-        alert("Nombre o contraseña incorrectas");
+        alert("User name or password incrorrect");
     }
 }
 function enter(e) {
@@ -15,14 +17,14 @@ function enter(e) {
     }
 }
 function loginindex(data) {
-    document.getElementById("login").style.display ="none";
+    document.getElementById("login").style.display = "none";
     switch (data) {
         case "teacher": {
-            document.getElementById("loginStudent").style.display ="block";            
+            document.getElementById("loginStudent").style.display = "block";
             break;
         }
         case "student": {
-            document.getElementById("loginTeacher").style.display ="block";                        
+            document.getElementById("loginTeacher").style.display = "block";
             break;
         }
         case "visitor": {
@@ -30,4 +32,19 @@ function loginindex(data) {
             break;
         }
     }
+}
+function goTo(route){
+    //Hide all sections and show the section selected
+    document.getElementById("event").style.display = "none";          
+    document.getElementById("time").style.display = "none";      
+    document.getElementById("category").style.display = "none";  
+    document.getElementById(route).style.display = "block";  
+
+    //Change css of sidebar (active class)
+    document.getElementById("nav_category").className = "nav_item"
+    document.getElementById("nav_time").className = "nav_item"
+    document.getElementById("nav_event").className = "nav_item"
+    document.getElementById("nav_"+route).className = "nav_item active"
+    
+    
 }
